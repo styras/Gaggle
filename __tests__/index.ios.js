d2ebbc739a13693a5e23b5708a2ced665c52e9f1
kjng <pianistkevinjang@gmail.com>
2017-02-20 11:03:12 -0800

    Update jest for nativebase testing

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,10 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
+import Signin from '../components/Signin/Signin.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+
 });
