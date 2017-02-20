3eeeae79a4c060e5705bba6bf984008ccdafe972
esstephan <ericastephan@gmail.com>
2017-02-20 12:25:37 -0800

    added ability to fetch dummy users from outside API call

--- components/GroupView/GroupView.js
+++ components/GroupView/GroupView.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Left, Right, Body, Footer, Content, Form, Item, Input, Icon, Button, Title, FooterTab } from 'native-base';
+import GetUsers from './GetUsers.js';
 
-export default class SecondPage extends Component {
+//ListView.dataSource
+//Fetch to get api data
+
+export default class GroupView extends Component {
   constructor(props, context) {
     super(props, context);
     this._handleChangePage = this._handleChangePage.bind(this);
+    this.state = {
+      users: []
+    };
   }
 
   _handleChangePage() {
     this.props.navigator.pop();
   }
 
+  componentDidMount() {
+    GetUsers.call(this);
+  }
+
   render() {
+
+    const userList = this.state.users.map((user) => {
+      return (
+        <View>
+          <Text> {'\u2022'} {user}</Text>
+          <Text></Text>
+        </View>
+      )
+    });
+
     return (
       <Container>
         <Header>
@@ -21,6 +42,9 @@ export default class SecondPage extends Component {
           <Right></Right>
         </Header>
         <Content>
+          <View>
+            {userList}
+          </View>
         </Content>
         <Footer>
           <FooterTab>
