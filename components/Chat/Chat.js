c5bbfc621e90a7ab0adec99d9a0cf715032e338f
kjng <pianistkevinjang@gmail.com>
2017-02-20 12:03:31 -0800

    Create default styling

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Left, Right, Body, Footer, Content, Form, Item, Input, Icon, Button, Title, FooterTab } from 'native-base';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Container, Header, Left, Right, Body, Footer, Content, Form, Item, Input, Icon, Button, Title, FooterTab, ListItem } from 'native-base';
 
 export default class NewComponent extends Component {
   constructor(props, context) {
@@ -21,16 +21,45 @@ export default class NewComponent extends Component {
           <Right></Right>
         </Header>
         <Content>
-          <Text>Heyo</Text>
+          <ListItem>
+            <Text>Kevin: </Text>
+            <Text>Hey guys</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Gwynn: </Text>
+            <Text>What's up?</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Erica: </Text>
+            <Text>Nothing much</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Maxwell: </Text>
+            <Text>Cool!</Text>
+          </ListItem>
         </Content>
         <Footer>
-          <FooterTab>
-            <Button onPress={this._handleChangePage}>
-              <Text>Go Back</Text>
+          <TextInput style={styles.textInput}
+            placeholder='Message'
+          />
+          <View style={{position: 'relative', top: 12}}>
+            <Button small>
+              <Text style={{color: 'white'}}>Send</Text>
             </Button>
-          </FooterTab>
+          </View>
+          <View style={{paddingRight: 10}}></View>
         </Footer>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textInput:{
+    flex:1,
+    borderColor: 'grey',
+    borderWidth: 1,
+    paddingLeft: 10,
+    margin: 10
+  }
+});
