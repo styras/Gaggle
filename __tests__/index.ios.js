281a41f8ff51f37b8444941200add00204165fe4
kjng <pianistkevinjang@gmail.com>
2017-02-21 09:53:21 -0800

    Update index snapshot test

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,6 +1,7 @@
 import 'react-native';
 import React from 'react';
 import Index from '../index.ios.js';
+import { firebaseRef, firebaseDB } from '../firebase/config.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
