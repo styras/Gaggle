5b31f48afb82c179f3d1b158f5d8fc8e809222d6
kjng <pianistkevinjang@gmail.com>
2017-02-22 23:21:00 -0800

    Update GroupView test

--- __tests__/GroupView.js
+++ __tests__/GroupView.js
@@ -1,19 +1,17 @@
 import 'react-native';
 import React from 'react';
 import GroupView from '../components/GroupView/GroupView.js';
-import {firebaseRef, firebaseDB} from '../firebase/firebaseHelpers';
+import { firebaseRef, firebaseDB } from '../firebase/firebaseHelpers';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-// Test is failing:
-//
-// it('it renders correctly', () => {
-//   const tree = renderer.create(
-//     <GroupView />
-//   );
-//   expect(tree).toMatchSnapshot();
-// });
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <GroupView />
+  );
+  expect(tree).toMatchSnapshot();
+});
 
 //SEEMS TO DO THE SAME THING...
 
