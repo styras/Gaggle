4a7cdb33cea95b5a35feff2e5351c33d33817356
kjng <pianistkevinjang@gmail.com>
2017-02-22 17:10:36 -0800

    Create custom rule allowing JSX in .js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,8 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    "rules": {
+      "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }]
+    }
 };
