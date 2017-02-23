eb9c84b7774e40f16c156efd75e34d32296e06e4
kjng <pianistkevinjang@gmail.com>
2017-02-23 15:41:19 -0800

    Clean up MapDisplay

--- components/MapDisplay/MapDisplay.js
+++ components/MapDisplay/MapDisplay.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Left, Right, Body, Footer, Content, Form, Item, Input, Icon, Button, Title, FooterTab } from 'native-base';
-import GetUsers from '../GroupView/GetUsers.js';
-import firebaseRef from '../../firebase/config.js';
-import firebase from 'firebase';
 import MapView from 'react-native-maps';
-
-const firebasedb = firebase.database();
 
 export default class MapDisplay extends Component {
   constructor(props) {
@@ -15,13 +8,15 @@ export default class MapDisplay extends Component {
 
   render() {
     return (
-      <MapView style={{width: 375, height: 375}}
-      initialRegion={{
-        latitude: this.props.user.location.coords.latitude,
-        longitude: this.props.user.location.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}/>
+      <MapView
+        style={{ width: 375, height: 375 }}
+        initialRegion={{
+          latitude: this.props.user.location ? this.props.user.location.coords.latitude : 0,
+          longitude: this.props.user.location ? this.props.user.location.coords.longitude : 0,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     );
   }
 }
