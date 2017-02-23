38e6e42de02b22461dc7a80c59a401d32a60160f
kjng <pianistkevinjang@gmail.com>
2017-02-22 22:54:56 -0800

    Auto scroll messages to bottom

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
-import { Container, Footer, Content, Icon, Button, ListItem, List, Text } from 'native-base';
-import { firebaseDB } from '../../firebase/firebaseHelpers';
+import { StyleSheet, View, TextInput, ListView } from 'react-native';
+import { Button, ListItem, List, Text } from 'native-base';
+import InvertibleScrollView from 'react-native-invertible-scroll-view';
 import moment from 'moment';
+import { firebaseDB } from '../../firebase/firebaseHelpers';
+
+const styles = StyleSheet.create({
+  textInput: {
+    flex: 1,
+    borderColor: 'grey',
+    borderWidth: 1,
+    paddingLeft: 10,
+    margin: 10,
+  },
+  chatInput: {
+    flex: 1,
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: 'lightgrey',
+  },
+});
 
 export default class Chat extends Component {
   constructor(props, context) {
@@ -13,15 +30,21 @@ export default class Chat extends Component {
       username: this.props.user ? this.props.user.displayName : 'Anonymous',
       input: '',
       group: this.props.groupName ? this.props.groupName : 'Default',
-      messages: []
-    }
+      messages: [],
+    };
 
+    this._ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this._chatList = {};
     this.messagesRef = this.database.ref('messages/' + this.state.group);
     this.sendMessage = this.sendMessage.bind(this);
   }
 
   componentDidMount() {
     this.messagesListener();
+  }
+
+  componentDidUpdate() {
+    this._chatList.scrollTo({y: 0});
   }
 
   _handleChangePage() {
@@ -32,9 +55,9 @@ export default class Chat extends Component {
     this.messagesRef.on('value', (snapshot) => {
       // Handle no messages created yet...
       if (snapshot.val() === null) {
-        this.setState({messages: []});
+        this.setState({ messages: [] });
       } else {
-        this.setState({messages: snapshot.val()});
+        this.setState({ messages: snapshot.val().reverse() });
       }
     });
   }
@@ -43,19 +66,18 @@ export default class Chat extends Component {
     // Write a message into database
     // Transaction will allow firebase to queue the requests so messages aren't written at the same time
     this.messagesRef.transaction((messages) => {
-      if (!messages) {
-        messages = [];
-      }
-      messages.push({
+      const groupMessages = messages || [];
+
+      groupMessages.push({
         name: this.state.username,
         message: this.state.input,
         timestamp: new Date().getTime()
       });
 
       // Clear TextInput
-      this.setState({input: ''});
+      this.setState({ input: '' });
 
-      return messages;
+      return groupMessages;
     });
   }
 
@@ -64,22 +86,31 @@ export default class Chat extends Component {
       <View>
         <View style={{height: 500}}>
           <Text>{JSON.stringify(this.props.user)}</Text>
-          <List dataArray={this.state.messages} renderRow={(obj) =>
-            <ListItem>
-              <Text style={{fontSize: 13}}>{obj.name} ({moment(obj.timestamp).fromNow()}): {obj.message}</Text>
-            </ListItem>
-          } />
+          <ListView
+            enableEmptySections
+            renderScrollComponent={props => <InvertibleScrollView {...props} inverted />}
+            ref={(chatList) => { this._chatList = chatList }}
+            dataSource={this._ds.cloneWithRows(this.state.messages)}
+            renderRow={obj =>
+              <ListItem>
+                <Text style={{ fontSize: 13 }}>
+                  {obj.name} ({moment(obj.timestamp).fromNow()}): {obj.message}
+                </Text>
+              </ListItem>
+            }
+          />
         </View>
         <View style={styles.chatInput}>
           <View style={{flex: 4, height: 50}}>
-            <TextInput style={styles.textInput}
+            <TextInput
+              style={styles.textInput}
               value={this.state.input}
-              onChangeText={(t) => this.setState({input: t})}
+              onChangeText={t => this.setState({ input: t })}
             />
           </View>
-          <View style={{flex: 1, marginTop: 10}}>
+          <View style={{ flex: 1, marginTop: 10 }}>
             <Button small onPress={this.sendMessage}>
-              <Text style={{color: 'white'}}>Send</Text>
+              <Text style={{ color: 'white' }}>Send</Text>
             </Button>
           </View>
         </View>
@@ -88,18 +119,7 @@ export default class Chat extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  textInput:{
-    flex:1,
-    borderColor: 'grey',
-    borderWidth: 1,
-    paddingLeft: 10,
-    margin: 10
-  },
-  chatInput: {
-    flex: 1,
-    flexDirection: 'row',
-    borderTopWidth: 1,
-    borderTopColor: 'lightgrey'
-  }
-});
+Chat.propTypes = {
+  user: React.PropTypes.object.isRequired,
+  groupName: React.PropTypes.string.isRequired,
+};
