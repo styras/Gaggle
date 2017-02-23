e13d044a3e42d7067ed598bde6b3a4d916badab1
maxuuell <maxuuell@gmail.com>
2017-02-23 15:26:10 -0800

    Add partial progress to firebase helper function

--- firebase/firebaseHelpers.js
+++ firebase/firebaseHelpers.js
@@ -1,12 +1,10 @@
 import ReactNative from 'react-native';
 import React from 'react';
-
 import firebase from 'firebase';
 import config from './config';
 
 export const firebaseRef = firebase.initializeApp(config);
 export const firebaseDB = firebaseRef.database();
-
 
 export function getUserId() {
   const user = firebaseRef.auth().currentUser;
@@ -28,4 +26,20 @@ export function updateUserLocation() {
     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
   );
 }
+
+export const addUserToGroup = (userObj, uid, groupName) => {
+  // expect a user object with
+  //{
+  //  displayName: String,
+  //  location: String,
+  //}
+  firebaseDB.ref(`groups/${groupName}/members/${uid}`).set(userObj);
+};
+
+export const getAllUsersInGroup = (groupName) => {
+  firebaseDB.ref(`groups/${groupName}/members`).once('value')
+    .then((snapshot) => {
+      return snapshot.val();
+    });
+};
 
