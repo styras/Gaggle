3c5e92b2d3c4b91b2b72c41b1f47d5566c6450f5
kjng <pianistkevinjang@gmail.com>
2017-02-23 12:42:43 -0800

    Lint GroupMapChat

--- components/GroupMapChat/GroupMapChat.js
+++ components/GroupMapChat/GroupMapChat.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Container, Content, Tab, Tabs, TabHeading, Icon, Text, Header, View } from 'native-base';
-import Chat from '../Chat/Chat.js';
-import MapDisplay from '../MapDisplay/MapDisplay.js';
+import { Container, Content, Tab, Tabs, TabHeading, Icon, Text, Header } from 'native-base';
+import Chat from '../Chat/Chat';
+import MapDisplay from '../MapDisplay/MapDisplay';
 
 export default class GroupMapChat extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: this.props.user
-    }
+      user: this.props.user,
+    };
   }
 
   render() {
@@ -18,7 +18,7 @@ export default class GroupMapChat extends Component {
         <Content scrollEnabled={false}>
           <Tabs>
             <Tab heading={<TabHeading><Icon name="compass" /><Text>Map</Text></TabHeading>}>
-              <MapDisplay />
+              <MapDisplay user={this.props.user} />
             </Tab>
             <Tab heading={<TabHeading><Icon name="chatboxes" /><Text>Chat</Text></TabHeading>}>
               <Chat user={this.state.user} />
@@ -26,6 +26,10 @@ export default class GroupMapChat extends Component {
           </Tabs>
         </Content>
       </Container>
-    )
+    );
   }
 }
+
+GroupMapChat.propTypes = {
+  user: React.PropTypes.object.isRequired,
+};
