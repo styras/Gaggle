df31e91d132c3075df9914283bdbf950d884380e
kjng <pianistkevinjang@gmail.com>
2017-02-24 14:52:17 -0800

    Create button function to handleClick

--- components/UberButton/UberButton.js
+++ components/UberButton/UberButton.js
@@ -3,13 +3,20 @@ import { Linking } from 'react-native';
 import { Container, Header, Text, Button } from 'native-base';
 
 export default class UberButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick() {
+    console.log('Button clicked');
+  }
 
   render() {
     return (
       <Container>
         <Header />
-        <Button><Text>UberButton</Text></Button>
+        <Button onPress={this.handleClick}><Text>UberButton</Text></Button>
       </Container>
     );
   }
