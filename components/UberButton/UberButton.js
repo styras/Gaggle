9ed207e28c2d8a569d3c987f6b520adc6864c3f4
kjng <pianistkevinjang@gmail.com>
2017-02-24 15:46:06 -0800

    Create functions for generating uber deep link and universal link based on current lat/lng and destination lat/lng

--- components/UberButton/UberButton.js
+++ components/UberButton/UberButton.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
 import { Linking } from 'react-native';
-import { Container, Header, Text, Button } from 'native-base';
+import { Container, Header, Text, Button, Input } from 'native-base';
+import { getUberDeepLink, getUberUniversalLink } from './uberLinks';
 
 export default class UberButton extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      destinationLatitude: 0,
+      destinationLongitude: 0,
+    };
+    this.currentLatitude = this.props.user.location.coords.latitude;
+    this.currentLongitude = this.props.user.location.coords.longitude;
   }
 
   handleClick() {
-    console.log('Button clicked');
+    const deepLink = getUberDeepLink(
+                      this.currentLatitude,
+                      this.currentLongitude,
+                      this.state.destinationLatitude,
+                      this.state.destinationLongitude,
+                    );
+    const universalLink = getUberUniversalLink(
+                            this.currentLatitude,
+                            this.currentLongitude,
+                            this.state.destinationLatitude,
+                            this.state.destinationLongitude,
+                          );
+    Linking.canOpenURL(deepLink).then((supported) => {
+      if (supported) {
+        console.log('openURL:', deepLink);
+        Linking.openURL(deepLink);
+      } else {
+        console.log('openURL:', universalLink);
+        Linking.openURL(universalLink);
+      }
+    });
   }
 
   render() {
     return (
       <Container>
         <Header />
+        <Text>Current: {this.currentLatitude}, {this.currentLongitude}</Text>
+        <Text>
+          Destination: {this.state.destinationLatitude}, {this.state.destinationLongitude}
+        </Text>
         <Button onPress={this.handleClick}><Text>UberButton</Text></Button>
       </Container>
     );
   }
 }
+
+UberButton.propTypes = {
+  user: React.PropTypes.object.isRequired,
+};
