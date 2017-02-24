331a9fb69ab888c9999127073482e43db660cfd1
kjng <pianistkevinjang@gmail.com>
2017-02-24 12:40:53 -0800

    Initialize UberButton

---
+++ components/UberButton/UberButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Container, Header, Text } from 'native-base';
+
+export default function UberButton() {
+  return (
+    <Container>
+      <Header />
+      <Text>UberButton</Text>
+    </Container>
+  );
+}
