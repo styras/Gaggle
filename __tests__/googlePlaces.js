752fd40413ff93214cb3e1441641dd351fb9cfe9
kjng <pianistkevinjang@gmail.com>
2017-02-25 13:10:52 -0800

    Create googlePlaces function and test

---
+++ __tests__/googlePlaces.js
@@ -0,0 +1,7 @@
+import getResultsFromCategory from '../google/googlePlaces';
+
+describe('getResultsFromCategory', () => {
+  it('is a function', () => {
+    expect(typeof getResultsFromCategory).toBe('function');
+  });
+});
---
+++ google/googlePlaces.js
@@ -0,0 +1,2 @@
+export default (locationArray, category) => {
+};
