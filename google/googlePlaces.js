17f2f0bc197087bfaca5846339a72cc1631cc2da
kjng <pianistkevinjang@gmail.com>
2017-02-25 14:05:12 -0800

    Build function and update test

--- google/googlePlaces.js
+++ google/googlePlaces.js
@@ -1,3 +1,17 @@
+import GOOGLE_API_KEY from './config';
+
 export default (locationArray, keyword) => {
-  return new Promise((resolve, reject) => {});
+  const latitude = locationArray[0];
+  const longitude = locationArray[1];
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=7500&keyword=${keyword}&key=${GOOGLE_API_KEY}`;
+
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        resolve(responseJson);
+      })
+      .catch(error => reject(error));
+  });
 };
