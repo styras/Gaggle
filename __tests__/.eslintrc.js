3f7d3c118322bf1f9948974504dd1d61b750a289
kjng <pianistkevinjang@gmail.com>
2017-02-25 08:50:16 -0800

    Create eslintrc for test env linting

---
+++ __tests__/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+      "browser": true,
+      "node": true,
+      "jest": true,
+    },
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+};
