a5b51311829906d0647a549f56bf820a99295b29
kjng <pianistkevinjang@gmail.com>
2017-02-27 16:01:39 -0800

    Create buttons to search For Me or For Group

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Text, Form, Item, Input, Button } from 'native-base';
 
+const styles = {
+  searchBar: {
+    position: 'relative',
+    top: -15,
+  },
+  searchInput: {
+    marginLeft: 10,
+  },
+};
+
 export default class Search extends Component {
   constructor(props) {
     super(props);
@@ -14,6 +24,17 @@ export default class Search extends Component {
       <Container>
         <Header />
         <Content>
+          <Header style={styles.searchBar} searchBar rounded>
+            <Item>
+              <Input style={styles.searchInput} placeholder="Search" />
+            </Item>
+            <Button transparent>
+              <Text>For Me</Text>
+            </Button>
+            <Button transparent>
+              <Text>For Group</Text>
+            </Button>
+          </Header>
           <Text>Heyo</Text>
         </Content>
       </Container>
