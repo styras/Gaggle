2692f992f90e6b244aa752664f6308bdbaeaf0f0
gwynndp <gwynndp@gmail.com>
2017-02-28 11:56:11 -0800

    Rework ListView rendering

--- components/Polls/Poll.js
+++ components/Polls/Poll.js
@@ -7,18 +7,18 @@ import Option from './Option';
 export default class Poll extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       username: this.props.user ? this.props.user.displayName : 'Anonymous',
       group: this.props.groupName ? this.props.groupName : 'Default',
-      dataSource: ds.cloneWithRows([
-        {text: 'John', votes: 9}, {text: 'Joel', votes: 4}, {text: 'James', votes: 1}, {text: 'Jimmy', votes: 3}, {text: 'Jackson', votes: 6}, {text: 'Jillian', votes: 2}, {text: 'Julie', votes: 5}, {text: 'Devin', votes: 3}
-      ]),
+      // dataSource: ds.cloneWithRows([
+      //   {text: 'John', votes: 9}, {text: 'Joel', votes: 4}, {text: 'James', votes: 1}, {text: 'Jimmy', votes: 3}, {text: 'Jackson', votes: 6}, {text: 'Jillian', votes: 2}, {text: 'Julie', votes: 5}, {text: 'Devin', votes: 3}
+      // ]),
       input: '',
-      options: [],
-
+      options: [
+         {text: 'John', votes: 9}, {text: 'Joel', votes: 4}, {text: 'James', votes: 1}, {text: 'Jimmy', votes: 3}, {text: 'Jackson', votes: 6}, {text: 'Jillian', votes: 2}, {text: 'Julie', votes: 5}, {text: 'Devin', votes: 3}
+      ],
     };
-    //bind functions to this
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.handleChecked = this.handleChecked.bind(this);
   }
 
@@ -43,7 +43,7 @@ export default class Poll extends Component {
         <Content>
           <View style={{flex: 1, paddingTop: 22}}>
             <ListView
-              dataSource={this.state.dataSource}
+              dataSource={this.ds.cloneWithRows(this.state.options)}
               renderRow={(rowData) =>
                 <Option text={rowData.text} votes={rowData.votes} handleChecked={this.handleChecked} />
               }
