18f00685e12bea5dd865613732de02f9774f25b3
kjng <pianistkevinjang@gmail.com>
2017-02-28 08:58:47 -0800

    Display distance between current location and a each place

--- components/Suggestions/Results.js
+++ components/Suggestions/Results.js
@@ -1,41 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { List, ListItem, Text } from 'native-base';
 import { Col, Grid } from 'react-native-easy-grid';
 import ResultDetails from './ResultDetails';
+import { getUserLocation, findDistanceBetweenCoords } from '../../location/locationHelpers';
 
-const Results = (props) => {
-  const goToResultDetails = (details) => {
-    props.navigator.push({
+export default class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myLocation: [],
+    };
+
+    this._getUserLocation();
+    this.goToResultDetails = this.goToResultDetails.bind(this);
+  }
+
+  goToResultDetails(details) {
+    this.props.navigator.push({
       component: ResultDetails,
       title: details.name,
       passProps: { placeId: details.place_id },
     });
-  };
+  }
 
-  return (
-    <List>
-      {props.results.map(result => (
-        <ListItem
-          key={result.id}
-          onPress={() => goToResultDetails(result)}
-        >
-          <Grid>
-            <Col size={10}>
-              <Text>{result.name}</Text>
-            </Col>
-            <Col size={1}>
-              <Text>1 mi</Text>
-            </Col>
-          </Grid>
-        </ListItem>
-      ))}
-    </List>
-  );
-};
+  _getUserLocation() {
+    const context = this;
+    navigator.geolocation.getCurrentPosition((position) => {
+      context.setState({
+        myLocation: [position.coords.latitude, position.coords.longitude],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <List>
+        {this.props.results.map(result => (
+          <ListItem
+            key={result.id}
+            onPress={() => this.goToResultDetails(result)}
+          >
+            <Grid>
+              <Col size={5}>
+                <Text>{result.name}</Text>
+              </Col>
+              <Col size={1}>
+                <Text>
+                  {findDistanceBetweenCoords(
+                      this.state.myLocation,
+                      [result.geometry.location.lat, result.geometry.location.lng],
+                    ).toFixed(2)} mi
+                </Text>
+              </Col>
+            </Grid>
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
+}
 
 Results.propTypes = {
   results: React.PropTypes.array.isRequired,
   navigator: React.PropTypes.object.isRequired,
 };
-
-export default Results;
