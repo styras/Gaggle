5682360afaa212d0966eae76b67c443e29f63ff2
esstephan <ericastephan@gmail.com>
2017-03-01 15:25:56 -0800

    clean up syntax and linting

--- components/MapDisplay/MapDisplay.js
+++ components/MapDisplay/MapDisplay.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
-import { firebaseRef, firebaseDB, updateUserLocation, getMemberLocations } from '../../firebase/firebaseHelpers';
-import { getUserLocation } from '../../location/locationHelpers.js';
-import duck_yellow from '../../images/duck_emoji_smaller.png';
-import duck_blue from '../../images/duck_emoji_smaller_blue.png';
-import duck_green from '../../images/duck_emoji_smaller_green.png';
-import duck_purple from '../../images/duck_emoji_smaller_purple.png';
-import duck_red from '../../images/duck_emoji_smaller_red.png';
+import { firebaseDB } from '../../firebase/firebaseHelpers';
+import { getUserLocation } from '../../location/locationHelpers';
+import duckYellow from '../../images/duck_emoji_smaller.png';
+import duckBlue from '../../images/duck_emoji_smaller_blue.png';
+import duckGreen from '../../images/duck_emoji_smaller_green.png';
+import duckPurple from '../../images/duck_emoji_smaller_purple.png';
+import duckRed from '../../images/duck_emoji_smaller_red.png';
 
 export default class MapDisplay extends Component {
   constructor(props) {
@@ -15,60 +15,67 @@ export default class MapDisplay extends Component {
     this.state = {
       currLoc: '',
       markersArray: [],
-    }
-  };
-
-  getMemberLocations(activeGroup) {
-    const context = this;
-    firebaseDB.ref(`groups/${activeGroup}/members/`).once('value', (snapshot) => {
-      snapshot.forEach((childSnapshot) => {
-        this.state.markersArray.push({coordinate: {
-          latitude: childSnapshot.val().location.coords.latitude,
-          longitude: childSnapshot.val().location.coords.longitude,
-          },
-          displayName: childSnapshot.val().displayName,
-        });
-      });
-      this.setState({
-        markersArray: this.state.markersArray,
-      })
-    });
+    };
   }
-
-
-  // getInitialState() {
-  //   return {
-  //     region: {
-  //       latitude: 37.78825,
-  //       longitude: -122.4324,
-  //       latitudeDelta: 0.0922,
-  //       longitudeDelta: 0.0421,
-  //     },
-  //   };
-  // }
-
-  // onRegionChange(region) {
-  //   this.setState({ region });
-  // }
 
   componentWillMount() {
     this.getMemberLocations(this.props.groupName);
     getUserLocation().then((response) => {
       this.setState({
         currLoc: response,
-      })
+      });
+    });
+  }
+
+  componentDidMount() {
+    const map = this.refs.mymap;
+    const context = this;
+    setTimeout(function () {
+      const markers = context.state.markersArray.map(function (marker) {
+        return marker.displayName;
+      });
+      map.fitToSuppliedMarkers(markers, false);
+    }, 2000);
+
+    setInterval(function () {
+      const markers = context.state.markersArray.map(function (marker) {
+        return marker.displayName;
+      });
+      getUserLocation()
+      .then((response) => {
+        context.setState({
+          currLoc: response,
+        });
+      });
+      map.fitToSuppliedMarkers(markers, false);
+    }, 10000);
+  }
+
+  getMemberLocations(activeGroup) {
+    firebaseDB.ref(`groups/${activeGroup}/members/`).once('value', (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        this.state.markersArray.push({ coordinate: {
+          latitude: childSnapshot.val().location.coords.latitude,
+          longitude: childSnapshot.val().location.coords.longitude,
+        },
+          displayName: childSnapshot.val().displayName,
+        });
+      });
+      this.setState({
+        markersArray: this.state.markersArray,
+      });
     });
   }
 
   render() {
     const { width, height } = Dimensions.get('window');
-    const emojis = [duck_yellow, duck_blue, duck_green, duck_purple, duck_red];
+    const emojis = [duckYellow, duckBlue, duckGreen, duckPurple, duckRed];
 
     return (
       <View>
         <MapView
           ref="mymap"
-          style={{width: width, height: height}}
+          style={{ width: width, height: height }}
           initialRegion={{
             latitude: this.state.currLoc ? this.state.currLoc[0] : 0,
             longitude: this.state.currLoc ? this.state.currLoc[1] : 0,
@@ -76,45 +83,22 @@ export default class MapDisplay extends Component {
             longitudeDelta: 0.0421,
           }}
         >
-        {this.state.markersArray.map((marker, i) => (
-            <MapView.Marker
-              key={i}
-              title={marker.displayName}
-              identifier={marker.displayName}
-              coordinate={{latitude: marker.coordinate.latitude, longitude: marker.coordinate.longitude}}
-              image={emojis[(5+i)%5]}
-            >
-            </MapView.Marker>
-          ))}
+          {this.state.markersArray.map((marker, i) => (
+          <MapView.Marker
+            key={i}
+            title={marker.displayName}
+            identifier={marker.displayName}
+            coordinate={{ latitude: marker.coordinate.latitude,
+              longitude: marker.coordinate.longitude }}
+            image={emojis[(5 + i) % 5]}
+          >
+          </MapView.Marker>
+        ))}
         </MapView>
       </View>
     );
   }
 
-  componentDidMount () {
-    var map = this.refs.mymap;
-    var context = this;
-    setTimeout(function(){
-      var markers = context.state.markersArray.map(function(marker){
-      return marker.displayName;
-      });
-      map.fitToSuppliedMarkers(markers, false);
-    }, 2000)
-
-    setInterval(function() {
-      var markers = context.state.markersArray.map(function(marker){
-      return marker.displayName;
-      });
-      getUserLocation()
-      .then((response) => {
-        context.setState({
-          currLoc: response,
-        })
-      })
-      map.fitToSuppliedMarkers(markers, false);
-      console.log('updated location of user')
-    }, 10000)
-  }
 }
 
 MapDisplay.propTypes = {
