e3c686bd2b65c136996b087b8344ae8ff9008b49
esstephan <ericastephan@gmail.com>
2017-03-04 14:31:07 -0800

    refactored getPhotoProps as separate function

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -36,6 +36,7 @@ export default class Search extends Component {
     this.handleSearchType = this.handleSearchType.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.getRandomCategory = this.getRandomCategory.bind(this);
+    this.getPhotoProps = this.getPhotoProps.bind(this);
 
     this.groupsSearches = firebaseDB.ref(`groups/${this.props.groupName}/searches`);
 
@@ -51,6 +52,7 @@ export default class Search extends Component {
         topSearches: topThreeSearches,
       });
     });
+
   }
 
   componentWillMount() {
@@ -87,35 +89,7 @@ export default class Search extends Component {
     getResultsFromKeyword(searchLocation, searchTerm, radius)
     .then((data) => {
       this.setState({ results: data.results, showInstructions: false, loading: false });
-      console.log('results are', data.results);
-
-      return new Promise(function (resolve, reject) {
-        const newResults = [];
-        this.state.results.forEach((result) => {
-          const photoref = result.photos ? result.photos[0].photo_reference : 'no_photo';
-          const newResult = result;
-          if (photoref === 'no_photo') {
-            newResult.photoURL = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQuDbG_i4uiHR5rOBuCttQTZ7TU-QBVcsHRu5PtqWeVvLDwRkkQlA';
-            newResults.push(newResult);
-          } else {
-            getPlacePhoto(photoref)
-            .then((response) => {
-              newResult.photoURL = response;
-              newResults.push(newResult);
-            });
-          }
-        })
-      .then(() => resolve(newResults))
-      .catch(error => reject(error));
-      })
-      .then((newResults) => {
-        this.setState({ results: newResults });
-      });
-        // setTimeout(() => {
-        //   console.log('newResults inside setTimeout', newResults);
-        //   this.setState({ results: newResults });
-        //   console.log('results are now', this.state.results);
-        // }, 4000);
+      this.getPhotoProps();
     });
   }
 
@@ -140,6 +114,30 @@ export default class Search extends Component {
       .then((data) => {
         this.setState({ results: data.results, showInstructions: false, loading: false });
       });
+  }
+
+  getPhotoProps() {
+    const newResults = [];
+    this.state.results.forEach((result) => {
+      console.log('getting photo');
+      const photoref = result.photos ? result.photos[0].photo_reference : 'no_photo';
+      const newResult = result;
+      // photoURL for results with no photoreference set to "photo not found" image
+      if (photoref === 'no_photo') {
+        newResult.photoURL = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQuDbG_i4uiHR5rOBuCttQTZ7TU-QBVcsHRu5PtqWeVvLDwRkkQlA';
+        newResults.push(newResult);
+      } else {
+        getPlacePhoto(photoref)
+        .then((response) => {
+          newResult.photoURL = response;
+          newResults.push(newResult);
+        });
+      }
+    });
+    setTimeout(() => {
+      this.setState({ results: newResults });
+      console.log('results are', this.state.results);
+    }, 5000);
   }
 
   _getUserLocation() {
