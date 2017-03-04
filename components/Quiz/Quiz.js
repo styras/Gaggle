3e664ebb127cf44ab11aab0cbb71c377c1291aaa
kjng <pianistkevinjang@gmail.com>
2017-03-04 15:22:50 -0800

    Import DeckSwiper for displaying questions

--- components/Quiz/Quiz.js
+++ components/Quiz/Quiz.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
-import { Container, Content, Header, Text } from 'native-base';
+import { Container, Content, Header, DeckSwiper, Card, CardItem, Body, Text, Button, Icon, H3 } from 'native-base';
 import getTriviaQuestions from '../../opentdb/opentdbHelpers';
 
 export default class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      question: {},
+      questions: [],
     };
 
     this.getNewQuestion();
   }
 
   getNewQuestion = () => {
-    getTriviaQuestions(1)
-      .then(results => {
-        const question = results[0];
-        this.setState({ question });
-      });
+    getTriviaQuestions(10)
+      .then(questions => this.setState({ questions }));
   }
 
   render() {
     return (
       <Container>
         <Header />
-        <Content>
-          <Text>{ JSON.stringify(this.state.question) }</Text>
+        <Content scrollEnabled={false}>
+          {this.state.questions.length > 0 &&
+          <DeckSwiper
+            dataSource={this.state.questions}
+            renderItem={question =>
+              <Card>
+                <CardItem>
+                  <Body>
+                    <H3 style={{ marginTop: 10, marginBottom: 10 }}>{question.category}</H3>
+                    <Text>{question.question}</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Text>{question.incorrect_answers}</Text>
+                </CardItem>
+              </Card>
+            }
+          />
+          }
+          <Text style={{ marginTop: 200 }}>{ JSON.stringify(this.state.questions) }</Text>
         </Content>
       </Container>
     );
