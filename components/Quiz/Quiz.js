bc0f1771c98daaff84cb0acde396d1e1a5ae616d
kjng <pianistkevinjang@gmail.com>
2017-03-04 15:04:25 -0800

    Display a trivia question object as text

--- components/Quiz/Quiz.js
+++ components/Quiz/Quiz.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { Container, Content, Header, Text } from 'native-base';
+import getTriviaQuestions from '../../opentdb/opentdbHelpers';
 
 export default class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: {},
+    };
+
+    this.getNewQuestion();
+  }
+
+  getNewQuestion = () => {
+    getTriviaQuestions(1)
+      .then(results => this.setState({ question: results[0] }));
+  }
+
   render() {
     return (
       <Container>
         <Header />
         <Content>
-          <Text>Quiz game here</Text>
+          <Text>{ JSON.stringify(this.state.question) }</Text>
         </Content>
       </Container>
     );
